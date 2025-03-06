e38f664f47b212744cf679fb1b2e4c5716e4fcb3
あああ <lanur622@gmail.com>
2025-03-06 13:25:45 +0900

    full effect with ripple

--- shaders/final_drafts/ripple_input_caustics_02.glsl
+++ shaders/final_drafts/ripple_input_caustics_02.glsl
@@ -3,15 +3,17 @@
 #iChannel3 "file://shaders/buffers/finite_approx_ripple_buffer.glsl"
 
 // debug macros
-//#define PIXELATE_UV
+#define PIXELATE_UV
 #define SHOW_NOISE_DISP_MAP_1
 #define SHOW_NOISE_DISP_MAP_2
 #define SHOW_CAUSTICS_DISP_MAP_1
 #define SHOW_CAUSTICS_DISP_MAP_2
 #define SHOW_BACKGROUND
+#define RIPPLE_EFFECT
 
 vec4 sample_disp_map(sampler2D tex, vec2 uv, vec2 velocity, vec2 positional_offset, float intensity_factor);
-vec4 sample_background_with_height_map(sampler2D tex, vec2 uv);
+vec4 sample_background_with_disp_map(sampler2D tex, vec2 uv, vec4 disp_map, float warp_factor);
+vec2 compute_ripple_offset(vec2 uv);
 float compute_effective_opacity(vec4 noise_disp_map_1, vec4 noise_disp_map_2, vec4 caustics_disp_map_1, vec4 caustics_disp_map_2);   
 vec2 scroll_displacement_map(vec2 uv, vec2 velocity, vec2 positional_offset); 
 vec2 pixelate_uv(vec2 uv);
@@ -26,6 +28,10 @@ void mainImage(out vec4 frag_color, in vec2 frag_coord) {
 
     #ifdef PIXELATE_UV
         uv = pixelate_uv(uv);
+    #endif
+
+    #ifdef RIPPLE_EFFECT
+        uv += compute_ripple_offset(uv);
     #endif
 
     #ifdef SHOW_NOISE_DISP_MAP_1
@@ -65,11 +71,8 @@ void mainImage(out vec4 frag_color, in vec2 frag_coord) {
     #endif
 
     #ifdef SHOW_BACKGROUND
-        background = sample_background_with_height_map(
-            iChannel1, uv 
-            //noise_disp_map_1,  
-            //BACKGROUND_DISP_WARP_FACTOR
-        );
+        //basic dicplacement map integration
+        background = sample_background_with_disp_map(iChannel1, uv, noise_disp_map_1, BACKGROUND_DISP_WARP_FACTOR);
     #endif
 
     float alpha = compute_effective_opacity(
@@ -95,7 +98,18 @@ vec4 sample_disp_map(
     return vec4(scaled_noise, scaled_noise, scaled_noise, 1.0);
 }
 
-//TODO THESE ARE MAYBE FUN TO FUCK WITH WITH AUDIO INPUT
+//BASIC DISPLACEMENT MAP EXAMPLE INTEGRATION WITHOUT SCROLLING
+vec4 sample_background_with_disp_map(
+    sampler2D tex, 
+    vec2 uv, 
+    vec4 disp_map, 
+    float warp_factor
+) {
+    vec2 bg_uv = uv + (disp_map.r * warp_factor);
+    return texture(tex, bg_uv);
+}
+
+//TODO: THESE ARE MAYBE FUN TO FUCK WITH WITH AUDIO INPUT
 #define NORMAL_SAMPLE_OFFSET     0.01 // How far we sample around uv to find local slope
 #define NORMAL_Z_SCALE           1.0  // Multiplier for the Z component in the normal
 #define AIR_REFRACTION_INDEX     1.0     
@@ -103,39 +117,32 @@ vec4 sample_disp_map(
 #define REFRACTION_INDEX_RATIO   (AIR_REFRACTION_INDEX/ WATER_REFRACTION_INDEX) // For water ~0.75. Try (1.0 / 1.5)=~0.67 (glass), (1.0 / 2.4)=~0.42 (diamond), etc.
 #define INCIDENT_DIRECTION       vec3(0.0, 0.0, -1.0) // "Camera is looking down" direction
 
-vec4 sample_background_with_height_map(
-    sampler2D background_tex,
-    vec2 uv
-    //vec2 disp_map, 
-    //float warp_factor
-) {
+vec2 compute_ripple_offset(vec2 uv) {
     // Wave simulation buffer (iChannel3) only stores a single "height" channel;
     // we sample neighbors here to derive slope (dX, dY).
     // The wave pass might use a different neighbor step (e.g. "EFFECTIVE_SAMPLE_STEP");
     // we choose NORMAL_SAMPLE_OFFSET here purely for rendering normals.
-
     float height_center = texture(iChannel3, uv).r;  // Might be unused if we only use neighbors
     float height_left   = texture(iChannel3, uv - vec2(NORMAL_SAMPLE_OFFSET, 0.0)).r;
     float height_right  = texture(iChannel3, uv + vec2(NORMAL_SAMPLE_OFFSET, 0.0)).r;
     float height_up     = texture(iChannel3, uv + vec2(0.0, NORMAL_SAMPLE_OFFSET)).r;
     float height_down   = texture(iChannel3, uv - vec2(0.0, NORMAL_SAMPLE_OFFSET)).r;
-
     float d_x = height_right - height_left;
     float d_y = height_up    - height_down;
-
     // --- 2. Build a 3D normal vector. 
     // Notice we multiply the Z part by NORMAL_Z_SCALE * NORMAL_SAMPLE_OFFSET
     // so we can control how "tilted" the surface is.
     vec3 raw_normal = vec3(d_x, d_y, NORMAL_Z_SCALE * NORMAL_SAMPLE_OFFSET);
     vec3 surface_normal = normalize(raw_normal);
-
     // --- 3. Refract the "incident direction" through this normal
     vec3 refracted = refract(INCIDENT_DIRECTION, surface_normal, REFRACTION_INDEX_RATIO);
-
-    vec2 refracted_offset = refracted.xy;// * warp_factor; //TODO figure out how to merge all the displacement layers with the ripple height map layer
-    vec2 bg_uv = uv + refracted_offset;
-    return texture(background_tex, bg_uv);
+    return refracted.xy;
 }
+
+// vec4 sample_background_with_height_map(sampler2D background_tex, vec2 uv) {
+//     vec2 bg_uv = uv + refracted_offset;
+//     return texture(background_tex, bg_uv);
+// }
 
 float compute_effective_opacity(
     vec4 noise_disp_map_1, 
@@ -157,7 +164,6 @@ float compute_effective_opacity(
     }
    return alpha;
 }
-
 
 vec2 scroll_displacement_map(vec2 uv, vec2 velocity, vec2 positional_offset) {
     return uv + iTime * velocity + positional_offset;
